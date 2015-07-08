38e02684f27ca8e8d57e1b6d47f136fad5c0b5fd
M.T <tech4.life@yahoo.com>
2015-07-08 13:44:47 +0200

    project tiles
    project tiles design and implementation

---
+++ backend/objects.js
@@ -0,0 +1,11 @@
+var projects=[
+{name:"veronica1",startDate:"25th jan 2011",endDate:"28th aug 2017"},
+{name:"veronica2",startDate:"25th jan 2011",endDate:"28th aug 2017"},
+{name:"veronica3",startDate:"25th jan 2011",endDate:"28th aug 2017"},
+{name:"veronica3",startDate:"25th jan 2011",endDate:"28th aug 2017"},
+{name:"veronica3",startDate:"25th jan 2011",endDate:"28th aug 2017"},
+{name:"veronica3",startDate:"25th jan 2011",endDate:"28th aug 2017"},
+];
+
+
+blocks.query({projects});
--- js/main.js
+++ js/main.js
@@ -108,8 +108,33 @@ $(document).ready(function () {
 
 	/************************ End of Document Events *****************************/
 
+	
+    
+});
+	/************************ End of Document Ready *****************************/
+
 	/************************************* Functions ******************************************/
 	/*this block should handle every function that can be invoked throughout whole application*/
 
-    
-})
+    //function to process hex color mid tone color
+	function padToTwo(numberString) {
+    if (numberString.length < 2) {
+        numberString = '0' + numberString;
+    }
+    return numberString;
+}
+
+function hexAverage() {
+    var args = Array.prototype.slice.call(arguments);
+    return args.reduce(function (previousValue, currentValue) {
+        return currentValue
+            .replace(/^#/, '')
+            .match(/.{2}/g)
+            .map(function (value, index) {
+                return previousValue[index] + parseInt(value, 16);
+            });
+    }, [0, 0, 0])
+    .reduce(function (previousValue, currentValue) {
+        return previousValue + padToTwo(Math.floor(currentValue / args.length).toString(16));
+    }, '#');
+}
